2a965a3039f6bdc9836d07ee21bcf25cb2f79e90
Brandon Nolan <brndalx@gmail.com>
2023-02-22 19:28:10 +0200

    feat: GET request to toys router, json items added

--- routes/toys.js
+++ routes/toys.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { ToysModel, validateJoi } = require("../models/toysModel");
+
+//In POSTMAN  change: request type to to GET, choose Body ===> row
+
+// GET request handle
+//Query example: http://localhost:3001/toys/?page=1&sort=name&desc=yes
+
 router.get("/", async (req, res) => {
-  res.json({ msg: "Toys route work" });
+  let perPage = 5;
+  // Variable to list the page
+  let page = req.query.page - 1 || 0;
+  // Variable to sort if not will use an mongo _id
+  let sort = req.query.sort || "_id";
+  // Variable to sort by acs or desc order
+  let desc = req.query.desc == "yes" ? 1 : -1;
+  try {
+    let data = await ToysModel.find({})
+      //makes display limit per page
+      .limit(perPage)
+      //helps to make two variables works together in order to achieve page listing
+      .skip(page * perPage)
+      //makes an possible to sort by choosen option
+      .sort({ [sort]: desc });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(502).json({ err });
+  }
 });
 
 module.exports = router;
