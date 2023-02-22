edd732565dc3418e0508012ab004ebdb77e5b3a9
Brandon Nolan <brndalx@gmail.com>
2023-02-22 19:17:04 +0200

    feat: added Joi validation model for toys route

--- models/toysModel.js
+++ models/toysModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const { date } = require("joi");
+
 //Definition for Schema to make request to the Mongo DB
 
 let schema = new mongoose.Schema({
@@ -14,4 +14,18 @@ let schema = new mongoose.Schema({
     default: Date.now,
   },
 });
+//TO DO: user_id for users
 exports.ToysModel = mongoose.model("toys", schema);
+// Joi validator to make checks that requests sended to DB are valid for created schema
+exports.validateJoi = (_reqBody) => {
+  //New schema object for valiation which contains all limitations for PUT and POST CRUD requests
+
+  let joiSchema = Joi.object({
+    name: Joi.string().min(2).max(150).required(),
+    info: Joi.string().min(2).max(150).required(),
+    category: Joi.string().min(2).max(150).required(),
+    img_url: Joi.string().min(2).max(150).allow(null, ""),
+    price: Joi.number().min(2).max(150).required(),
+  });
+  return joiSchema.validate(_reqBody);
+};
