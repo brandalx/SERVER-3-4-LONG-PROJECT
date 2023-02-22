509441e3b3a13ac1e0513e5663dc7533964b05b7
Brandon Nolan <brndalx@gmail.com>
2023-02-22 19:19:41 +0200

    feat: import toys model into toys router

--- routes/toys.js
+++ routes/toys.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { ToysModel, validateJoi } = require("../models/toysModel");
 router.get("/", async (req, res) => {
   res.json({ msg: "Toys route work" });
 });
