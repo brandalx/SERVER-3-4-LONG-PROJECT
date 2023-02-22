066fe986340fa0918d029e4707f54bc3349abdd9
Brandon Nolan <brndalx@gmail.com>
2023-02-22 18:44:09 +0200

    feat: 404 response added

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -4,6 +4,8 @@ const usersRoute = require("./users");
 exports.routesInit = (app) => {
   app.use("/main", indexRoute);
   app.use("/users", usersRoute);
+  app.use((req, res) => {
+    // res.status(404).send("Sorry, page not found.");
+    res.status(404).json({ msg: " The page you required was not found" });
+  });
 };
-
-//To do: 404 route
