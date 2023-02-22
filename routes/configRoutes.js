3591b9226b671a9098c03b2564bcb437ebf7eab3
Brandon Nolan <brndalx@gmail.com>
2023-02-22 19:04:51 +0200

    fix: SEO 404 fix

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -9,7 +9,7 @@ exports.routesInit = (app) => {
   app.use((req, res) => {
     // res.status(404).send("Sorry, page not found.");
     res.status(404).json({
-      msg: " The page you required was not found. If you are lost, follow to index.html",
+      msg: "Error 404: The page you are looking for could not be found. Please check the URL and try again. If you're not sure where to go, you can always head back to the homepage by entering in the domain/index.html",
     });
   });
 };
