f909fa090b042df9f3ba64f656fc0edac5859232
Brandon Nolan <brndalx@gmail.com>
2023-02-23 02:02:41 +0200

    feat: token generation added for users router

--- models/usersModel.js
+++ models/usersModel.js
@@ -38,3 +38,11 @@ exports.validateLogin = (_reqBody) => {
   });
   return joiSchema.validate(_reqBody);
 };
+
+exports.createToken = (user_id) => {
+  //          ===>           PLEASE MENTION: [THIS] KEY WILL BE CHANGED WHEN WILL BE DEPLOYED TO THE REAL SERVER
+  //                                           \/
+  //                       payload    | the secret key |  options object (will expire in 60 minutes)
+  let token = jwt.sign({ _id: user_id }, "secretWord", { expiresIn: "60mins" });
+  return token;
+};
