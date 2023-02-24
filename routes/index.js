3ce9141374badfb8c1f79429f79e5f482093e45e
Brandon Nolan <brndalx@gmail.com>
2023-02-25 00:51:30 +0200

    feat: deployment inforamtion update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 router.get("/", async (req, res) => {
-  res.json({ msg: "Homepage. If you are lost, follow to index.html" });
+  res.json({
+    msg: "Homepage. If you are lost, follow to https://toysrestapi.cyclic.app/",
+  });
 });
 
 module.exports = router;
