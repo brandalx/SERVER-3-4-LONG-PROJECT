2614d862645d93a59f1cd31da36ed46d79f37763
Brandon Nolan <brndalx@gmail.com>
2023-03-03 06:44:15 +0200

    feat: swagger automated docs

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,3 +1,4 @@
+//for /toys GET request
 /**
  * @swagger
  * /toys:
@@ -58,6 +59,63 @@
  *                   date_created: "2023-02-24T23:24:19.906Z"
  *                   user_id: "63f945d1be82167cccb62037"
  *                   __v: 0
+ *       '502':
+ *         description: Internal server error.
+ */
+
+//for /toys/search GET request
+
+/**
+ * @swagger
+ * /toys/search:
+ *   get:
+ *     summary: Returns a paginated list of toys that match the search query
+ *     description: Returns a paginated list of toys that match the search query provided in the `s` query parameter. The search is performed on the `name` and `info` fields of the toy objects.
+ *     tags:
+ *       - Toys
+ *     parameters:
+ *       - in: query
+ *         name: s
+ *         required: true
+ *         description: The search query to retrieve toys.
+ *         schema:
+ *           type: string
+ *
+ *     responses:
+ *       '200':
+ *         description: A list of toys.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 toys:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                 page:
+ *                   type: integer
+ *                   minimum: 1
+ *                 totalPages:
+ *                   type: integer
+ *                   minimum: 1
+ *                 totalToys:
+ *                   type: integer
+ *                   minimum: 0
+ *             example:
+ *               toys:
+ *                 - _id: "6122b3a1c0a534a8ca8cbb21"
+ *                   name: "Teddy bear"
+ *                   info: "This 3-story dollhouse features a working elevator, pool, and garage."
+ *                   category: "Dolls & Accessories"
+ *                   img_url: "https://images-na.ssl-images-amazon.com/images/I/71x7xLzk%2B4L._AC_SL1000_.jpg"
+ *                   price: 20
+ *                   date_created: "2023-02-24T23:24:19.906Z"
+ *                   user_id: "63f945d1be82167cccb62037"
+ *                   __v: 0
+ *               page: 1
+ *               totalPages: 1
+ *               totalToys: 1
  *       '502':
  *         description: Internal server error.
  */
